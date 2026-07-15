d940c322833a31a1d737875204eb8d93373bc364
Neetu8354 <neet.u8354@gmail.com>
2026-07-15 16:05:58 +0530

    Fix site blinking by disabling redirect loop

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,9 @@ import { createRoot } from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
 import App from "./App.tsx";
 import "./index.css";
-import { enforceCanonicalHost } from "./lib/canonicalHost";
+// import { enforceCanonicalHost } from "./lib/canonicalHost";
 
-enforceCanonicalHost();
+// enforceCanonicalHost(); // Disabled to prevent redirect loop
 
 createRoot(document.getElementById("root")!).render(
   <HelmetProvider>
